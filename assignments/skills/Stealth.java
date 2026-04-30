80ae8c306c53a3e1f062e4bff83dc4fdcb47a14b
b24fm1032 <emvjn44@gmail.com>
2026-04-30 08:31:17 -0500

    Add constructor and cast method to Stealth class
    Implemented constructor and cast method for Stealth class.

--- assignments/skills/Stealth.java
+++ assignments/skills/Stealth.java
@@ -1,11 +1,15 @@
 public class Stealth extends Skill {
 
-    // TODO: Constructor бичнэ үү
-    // public Stealth()
-    // - super("Stealth", 10) дуудна
+    public Stealth() {
+        super("Stealth", 10);
+    }
 
-    // TODO: @Override cast(Character caster, Character target) → void
-    // - Хохирол учруулахгүй (харанхуйд нуугдана)
-    // - caster.mp -= 10
-
+    @Override
+    public void cast(Character caster, Character target) {
+    
+    
+        caster.mp -= 10;
+        
+        System.out.println(caster.getName() + " fades into the shadows using Stealth!");
+    }
 }
