ccc7736e36460e7b9a28b7f8a73a06803ad5c2aa
b24fm1032 <emvjn44@gmail.com>
2026-04-30 08:25:43 -0500

    Implement use method for Potion class

--- assignments/skills/Potion.java
+++ assignments/skills/Potion.java
@@ -1,6 +1,10 @@
 public class Potion implements Usable {
 
-    // TODO: @Override use(Character user) → void
-    // - user.heal(50)
-
+    @Override
+    public void use(Character user) {
+      
+        user.heal(50);
+        
+        System.out.println(user.getName() + " used a Potion and recovered 50 HP!");
+    }
 }
